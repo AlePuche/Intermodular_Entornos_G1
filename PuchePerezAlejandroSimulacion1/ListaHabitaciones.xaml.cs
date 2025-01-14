585b44b70f2426495eed568a18d210b9e7279fce
AlePuche <alepucheperez@gmail.com>
2025-01-14 17:01:56 +0100

    Corregiga dirección del scroll vertical

--- PuchePerezAlejandroSimulacion1/ListaHabitaciones.xaml.cs
+++ PuchePerezAlejandroSimulacion1/ListaHabitaciones.xaml.cs
@@ -31,10 +31,10 @@ namespace PuchePerezAlejandroSimulacion1
         {
             Habitaciones = new ObservableCollection<Habitacion>
             {
-                new Habitacion { Id = 115, FotoUrl = "/Images/usuario.png", Tipo = "Habitación Doble", Huespedes = 4, Descripcion = "Amplia y cómoda", Reservada = "Sí" },
-                new Habitacion { Id = 116, FotoUrl = "/Images/usuario.png", Tipo = "Habitación Simple", Huespedes = 1, Descripcion = "Con vistas al mar", Reservada = "No" },
-                new Habitacion { Id = 115, FotoUrl = "/Images/usuario.png", Tipo = "Habitación Doble", Huespedes = 4, Descripcion = "Amplia y cómoda", Reservada = "Sí" },
-                new Habitacion { Id = 116, FotoUrl = "/Images/usuario.png", Tipo = "Habitación Simple", Huespedes = 1, Descripcion = "Con vistas al mar", Reservada = "No" },
+                new Habitacion { Id = 102, FotoUrl = "/Images/usuario.png", Tipo = "Habitación Doble", Huespedes = 4, Descripcion = "Amplia y cómoda", Reservada = "Sí" },
+                new Habitacion { Id = 103, FotoUrl = "/Images/usuario.png", Tipo = "Habitación Simple", Huespedes = 1, Descripcion = "Con vistas al mar", Reservada = "No" },
+                new Habitacion { Id = 104, FotoUrl = "/Images/usuario.png", Tipo = "Habitación Doble", Huespedes = 4, Descripcion = "Amplia y cómoda", Reservada = "Sí" },
+                new Habitacion { Id = 105, FotoUrl = "/Images/usuario.png", Tipo = "Habitación Simple", Huespedes = 1, Descripcion = "Con vistas al mar", Reservada = "No" },
                 new Habitacion { Id = 115, FotoUrl = "/Images/usuario.png", Tipo = "Habitación Doble", Huespedes = 4, Descripcion = "Amplia y cómoda", Reservada = "Sí" },
                 new Habitacion { Id = 116, FotoUrl = "/Images/usuario.png", Tipo = "Habitación Simple", Huespedes = 1, Descripcion = "Con vistas al mar", Reservada = "No" },
                 new Habitacion { Id = 115, FotoUrl = "/Images/usuario.png", Tipo = "Habitación Doble", Huespedes = 4, Descripcion = "Amplia y cómoda", Reservada = "Sí" },
@@ -52,5 +52,7 @@ namespace PuchePerezAlejandroSimulacion1
                 new Habitacion { Id = 116, FotoUrl = "/Images/usuario.png", Tipo = "Habitación Simple", Huespedes = 1, Descripcion = "Con vistas al mar", Reservada = "No" }
             };
         }
+
+        
     }
 }
